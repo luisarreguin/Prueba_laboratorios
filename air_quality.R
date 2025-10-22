0f46d7985c9b49990b7e34c38dfac7b9326b430b
texcalac <texcalac@gmail.com>
2025-10-22 13:44:41 -0600

    clean and scatter

--- air_quality.R
+++ air_quality.R
@@ -1,6 +1,13 @@
 library(tidyverse)
+library(janitor)
 
-aire <- 
-  airquality %>% 
-  as_tibble() %>% 
+aire <-
+  airquality %>%
+  as_tibble() %>%
+  clean_names() %>%
   print()
+
+# Scatter plot de ozono vs temp
+ggplot(aire, aes(ozone, temp)) +
+  geom_point() +
+  theme_bw()
